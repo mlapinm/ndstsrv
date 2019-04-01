3e23c96ff679d6f7ed8e41a552090e2928dc0b18
Mikhail Lapin <mlapin@rambler.ru>
2019-04-01 19:51:00 +0300

    new static server

---
+++ server/server.js
@@ -0,0 +1,9 @@
+
+var static=require('node-static')
+var file = new static.Server()
+require('http').createServer(function(req,res){
+    req.addListener('end',function(){
+        file.serve(req, res)
+    }).resume()
+}).listen(process.env.PORT || 3000)
+
